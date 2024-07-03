59c1be33fea74f7583d50d9e1278c377d432ca35
florian-saraka-mac <florian-saraka-mac@bihar.com>
2024-07-03 14:19:41 +0000

    remove profile in the matcher

--- middleware.js
+++ middleware.js
@@ -2,5 +2,5 @@ export { default } from "next-auth/middleware";
 
 
 export const config = {
-    matcher: ["/profile", "/events/create", "/events/:id/update"]
+    matcher: ["/events/create", "/events/:id/update"]
 }
